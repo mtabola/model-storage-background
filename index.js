483d8b81f335037b0f120f9ffa2ce976cb829f2b
Mikhail Tabola <m-tabola@mail.ru>
2022-12-15 14:37:33 +0300

    [FIX] delete unused lib

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 const http = require('http')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
